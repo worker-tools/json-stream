f487754cb13d12e8a2e23c11b53dca3811ebdce9
Florian Klampfer <mail@qwtel.com>
2022-05-08 15:41:12 +0700

    Commit

--- json-stringify-stream.ts
+++ json-stringify-stream.ts
@@ -1,35 +1,23 @@
 // deno-lint-ignore-file no-explicit-any
-import { ResolvablePromise } from 'https://ghuc.cc/worker-tools/resolvable-promise/index.ts'
-import { ExtendablePromise } from 'https://ghuc.cc/worker-tools/extendable-promise/index.ts'
 import { jsonStringifyGenerator } from './json-stringify.ts'
 
 export class JSONStringifyStream extends TransformStream<any, string> {
   constructor() {
     let first: boolean;
-    let flushed: ResolvablePromise<void>;
-    let done: ExtendablePromise<void>;
     super({
       start(controller) {
         first = true;
-        flushed = new ResolvablePromise();
-        done = new ExtendablePromise(flushed); 
         controller.enqueue('[')
       },
-      transform(obj, controller) {
+      async transform(obj, controller) {
         if (!first) controller.enqueue(','); else first = false;
-        const p = (async () => {
-          for await (const chunk of jsonStringifyGenerator(obj)) {
-            controller.enqueue(chunk)
-          }
-        })()
-        done.waitUntil(p)
-        return p;
+        for await (const chunk of jsonStringifyGenerator(obj)) {
+          controller.enqueue(chunk)
+        }
       },
-      async flush(controller) {
-        flushed.resolve();
-        await done; // FIXME: is this even necessary?
+      flush(controller) {
         controller.enqueue(']')
-      }
+      },
     })
   }
 }
