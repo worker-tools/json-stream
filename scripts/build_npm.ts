9d5c2fc5c5e0d065a6d16bf9b7d1c7bb2e9762ae
Florian Klampfer <mail@qwtel.com>
2022-05-27 18:08:10 +0700

    Commit

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -1,12 +1,10 @@
 #!/usr/bin/env -S deno run --allow-read --allow-write=./,/Users/qwtel/Library/Caches/deno --allow-net --allow-env=HOME,DENO_AUTH_TOKENS,DENO_DIR --allow-run=git,pnpm
-// deno-lint-ignore-file no-unused-vars
 
-// ex. scripts/build_npm.ts
 import { basename, extname } from "https://deno.land/std@0.133.0/path/mod.ts";
 import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
 
 import { 
-  latestVersion, copyMdFiles, getDescription, getGHTopics, getGHLicense, getGHHomepage,
+  copyMdFiles, mkPackage,
 } from 'https://gist.githubusercontent.com/qwtel/ecf0c3ba7069a127b3d144afc06952f5/raw/latest-version.ts'
 
 await emptyDir("./npm");
@@ -18,25 +16,12 @@ await build({
   outDir: "./npm",
   shims: {},
   test: false,
-  package: {
-    // package.json properties
-    name: `@worker-tools/${name}`,
-    version: await latestVersion(),
-    description: await getDescription(),
-    license: await getGHLicense(name) ?? 'MIT',
-    publishConfig: {
-      access: "public"
-    },
-    author: "Florian Klampfer <mail@qwtel.com> (https://qwtel.com/)",
-    repository: {
-      type: "git",
-      url: `git+https://github.com/worker-tools/${name}.git`,
-    },
-    bugs: {
-      url: `https://github.com/worker-tools/${name}/issues`,
-    },
-    homepage: await getGHHomepage(name) ?? `https://github.com/worker-tools/${name}#readme`,
-    keywords: await getGHTopics(name) ?? [],
+  package: await mkPackage(name),
+  declaration: true,
+  packageManager: 'pnpm',
+  compilerOptions: {
+    sourceMap: true,
+    target: 'ES2019',
   },
   mappings: {
     'https://cdn.skypack.dev/ts-functional-pipe@3.1.2?dts': {
@@ -55,11 +40,6 @@ await build({
       name: 'whatwg-stream-to-async-iter',
       version: 'latest',
     },
-  },
-  packageManager: 'pnpm',
-  compilerOptions: {
-    sourceMap: true,
-    target: 'ES2019'
   },
 });
 
